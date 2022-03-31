ce22bbc3f37075238076406d3ed4fdeec4e44359
Maciej Jaszczuk <git@artprotect.pl>
2022-03-31 23:13:45 +0200

    Login-complete

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('/login');
+        return; //jeśli nie ma sesji trzeba zatrzymać funkcję żeby nie robiła kolejnych roterów
+    }
+    next(); //jeśli sesja istnieje to przechodzi do admin - pamieta 24 godziny
+})
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('admin', { title: 'Admin' });
+    res.render('admin', { title: 'Admin' });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const login = 'admin';
+const password = 'maciek3003';
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+/* GET LOGOWANIE _ FORM */
+router.get('/login', (req, res, next) => {
+  res.render('login', { title: 'Logowanie' });
+});
+
+/* POST LOGOWANIE PRZEJĘCIE DANYCH */
+router.post('/login', (req, res) => {
+  const body = req.body;
+  if (body.login === login && body.password === password) {
+
+    req.session.admin = 1;  //Wywołanie sesji
+
+    res.redirect('/admin');
+  }else {
+    res.redirect('/login');
+  }
+  
 });
 
 module.exports = router;
