9234b3f156168134752e0bc835c18e6810145fd8
Maciej Jaszczuk <git@artprotect.pl>
2022-04-02 15:08:04 +0200

    master

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: 'mongodb+srv://monk101st:maciek3003@artprotect.6c3dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    db: 'mongodb+srv://monk101st:maciek3003@artprotect.6c3dy.mongodb.net/myFirstDatabase?retryWrites=true',
     keySession: ['TWOJKLUCZ'],
-    maxAgeSession: 24 * 60 * 60 * 1000  //24 godziny
+    maxAgeSession: 24 * 60 * 60 * 1000   //24 godziny
 }
